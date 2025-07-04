76bf3fb232cfbc954cb19a7b531b1cd0c69cf2ef
Anupam1707 <programmer.tiak@gmail.com>
2025-07-04 12:48:30 +0530

    Dashboard Scratch

---
+++ streamlit-dashboard/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title="Stock Forecast Dashboard", layout="wide")
+
+st.title("📈 Stock Market Forecast Dashboard")
